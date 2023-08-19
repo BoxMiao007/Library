857bf4926e6f6a1f52daa1cb1f9584544b4619ad
BoxMiao007 <92012474+BoxMiao007@users.noreply.github.com>
2023-08-19 13:25:39 +0800

    Update py_to_C++.py

--- python to C++/py_to_C++.py
+++ python to C++/py_to_C++.py
@@ -2,66 +2,66 @@ import os
 import shutil
 import subprocess
 
-def list_files_with_extension(directory, extension):
-    return [file for file in os.listdir(directory) if file.endswith(extension)]
+def 列出指定后缀文件(directory, 后缀):
+    return [file for file in os.listdir(directory) if file.endswith(后缀)]
 
-def select_file(files, prompt_message):
-    print(prompt_message)
+def 选择文件(files, 提示信息):
+    print(提示信息)
     for idx, file in enumerate(files, start=1):
         print(f"{idx}. {file}")
-    choice = int(input("Please enter the number of your choice: ")) - 1
+    choice = int(input("请输入您的选择编号: ")) - 1
     return files[choice]
 
-def process_script(source_file, target_file, icon_file):
+def 处理脚本(源文件, 目标文件, 图标文件):
     try:
-        with open(source_file, "r", encoding="utf-8") as source:
-            content = source.read()
+        with open(源文件, "r", encoding="utf-8") as f:
+            内容 = f.read()
     except FileNotFoundError:
-        content = ""
+        内容 = ""
     
-    if 'import sys' not in content:
-        content = 'import sys\nimport os\n\n' + content
-        with open(target_file, "w", encoding="utf-8") as target:
-            target.write(content)
-            print(f"Created and modified {target_file}")
+    if 'import sys' not in 内容:
+        内容 = 'import sys\nimport os\n\n' + 内容
+        with open(目标文件, "w", encoding="utf-8") as f_target:
+            f_target.write(内容)
+            print(f"已创建并修改 {目标文件}")
 
-    packaging_command = f'nuitka --windows-icon-from-ico={icon_file} {target_file}'
-    subprocess.run(packaging_command, shell=True, check=True)
-    print(f"Packaged {target_file} into an executable using Nuitka.")
+    打包命令 = f'nuitka --windows-icon-from-ico={图标文件} {目标文件}'
+    subprocess.run(打包命令, shell=True, check=True)
+    print(f"已使用 Nuitka 打包 {目标文件} 为可执行文件。")
 
-def delete_folder_or_file(path):
+def 删除文件夹或文件(path):
     if os.path.exists(path):
         if os.path.isdir(path):
             shutil.rmtree(path)
-            print(f"Deleted {path}")
+            print(f"已删除 {path}")
         else:
             os.remove(path)
-            print(f"Deleted {path}")
+            print(f"已删除 {path}")
 
-def main():
-    current_directory = "."
-    py_file_list = list_files_with_extension(current_directory, ".py")
-    ico_file_list = list_files_with_extension(current_directory, ".ico")
+def 主函数():
+    当前目录 = "."
+    py文件列表 = 列出指定后缀文件(当前目录, ".py")
+    ico文件列表 = 列出指定后缀文件(当前目录, ".ico")
 
-    selected_py_file = select_file(py_file_list, "Please select the Python script to package:")
-    selected_ico_file = select_file(ico_file_list, "Please select an icon file:")
+    选择的py文件 = 选择文件(py文件列表, "请选择要打包的 Python 脚本:")
+    选择的ico文件 = 选择文件(ico文件列表, "请选择一个图标文件:")
 
-    target_script_file = os.path.join(os.path.dirname(selected_py_file), os.path.basename(selected_py_file).replace(".py", "-to-c++.py"))
-    shutil.copy(selected_py_file, target_script_file)
-    process_script(target_script_file, target_script_file, selected_ico_file)
+    目标脚本文件 = os.path.join(os.path.dirname(选择的py文件), os.path.basename(选择的py文件).replace(".py", "-to-c++.py"))
+    shutil.copy(选择的py文件, 目标脚本文件)
+    处理脚本(目标脚本文件, 目标脚本文件, 选择的ico文件)
 
-    build_folder = os.path.splitext(target_script_file)[0] + ".build"
-    cmd_file = f"{os.path.splitext(target_script_file)[0]}.cmd"
+    build文件夹 = os.path.splitext(目标脚本文件)[0] + ".build"
+    cmd文件 = f"{os.path.splitext(目标脚本文件)[0]}.cmd"
 
-    delete_all = input("Do you want to delete the generated .build folder and .cmd file? (y/n): ")
-    if delete_all.lower() == "y":
-        delete_folder_or_file(build_folder)
-        delete_folder_or_file(cmd_file)
+    删除全部 = input("是否删除生成的 .build 文件夹和 .cmd 文件？(y/n): ")
+    if 删除全部.lower() == "y":
+        删除文件夹或文件(build文件夹)
+        删除文件夹或文件(cmd文件)
 
-    generated_exe_file = os.path.splitext(target_script_file)[0] + ".exe"
-    renamed_exe_file = generated_exe_file.replace("-to-c++", "")
-    os.rename(generated_exe_file, renamed_exe_file)
-    print(f"Renamed {generated_exe_file} to {renamed_exe_file}")
+    生成的exe文件 = os.path.splitext(目标脚本文件)[0] + ".exe"
+    重命名exe文件 = 生成的exe文件.replace("-to-c++", "")
+    os.rename(生成的exe文件, 重命名exe文件)
+    print(f"已重命名 {生成的exe文件} 为 {重命名exe文件}")
 
 if __name__ == "__main__":
-    main()
+    主函数()
